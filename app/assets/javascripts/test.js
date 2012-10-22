375678a722859d9ed5e46bf4a930b05cddec3061
Ellen <ellenw@sfu.ca>
2012-10-22 11:19:04 -0700

    simple collision detection

--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -5,21 +5,65 @@ $(function() {
   $(document).keydown(function(e){
       if (e.keyCode == 37) { //left
          playerObject.x -= 0.1;
+		 if (collision_detect(playerObject, testObject2)){
+			playerObject.x += 0.1;
+			}
          return false;
       }
       if (e.keyCode == 38) { //up
          playerObject.y += 0.1;
+		 if (collision_detect(playerObject, testObject2)){
+			playerObject.y -= 0.1;
+			}
          return false;
       }
       if (e.keyCode == 39) { //right
          playerObject.x += 0.1;
+		 if (collision_detect(playerObject, testObject2)){
+			playerObject.x -= 0.1;
+			}
          return false;
       }
       if (e.keyCode == 40) { //down
          playerObject.y -= 0.1;
+		 if (collision_detect(playerObject, testObject2)){
+			playerObject.y += 0.1;
+			}
          return false;
       }
   });
+  
+  var collision_detect = function(obj1, obj2) {
+	
+	distx1 = Math.abs(obj1.x) + obj1.width/2;
+	disty1 = Math.abs(obj1.y) + obj1.height/2;
+	distx2 = Math.abs(obj2.x) + obj2.width/2;
+	disty2 = Math.abs(obj2.y) + obj2.height/2;
+
+	a = Math.abs(obj1.x) + Math.abs(obj2.x);
+	b = Math.abs(obj1.y) + Math.abs(obj2.y);
+	
+	hypo = Math.sqrt(a*a + b*b);
+	
+	if (hypo <= (distx1 + distx2)/2){
+		console.log(distx1, disty1, distx2, disty2);
+		console.log(distx1-distx2, disty1-disty2, hypo);
+		console.log("Collision1.");
+		return true;
+	}	
+
+	if (hypo <= (disty1 + disty2)/2){
+		console.log(distx1, disty1, distx2, disty2);
+		console.log(distx1-distx2, disty1-disty2, hypo);
+		console.log("Collision2.");
+		return true;
+	}	
+
+	console.log(distx1, disty1, distx2, disty2);
+	console.log(distx1-distx2, disty1-disty2);
+	console.log("No collision.");
+	return false;
+}
   initRenderer();
 
   var testObject = new GameObject();
