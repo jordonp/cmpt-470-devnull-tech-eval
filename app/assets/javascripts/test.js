00b6e9b4c5ec62de6f0fce74b606daab116ead8e
Jordon Phillips <jordonph@gmail.com>
2012-10-22 22:15:05 -0700

    added support for untextured solid-color objects

--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -68,14 +68,15 @@ $(function() {
 
   var testObject = new GameObject();
   testObject.z = -4.0;
-  testObject.x = -0.5;
+  testObject.x = -2.5;
   testObject.height = 2.0;
   testObject.color = [0.0, 0.2, 0.2];
 
   var testObject2 = new GameObject();
   testObject2.z = -4.0;
   testObject2.width = 2.0;
-  testObject2.color = [0.2, 0.2, 0.0];
+  testObject2.color = [1.0, 1.0, 0.6];
+  testObject2.setTexture("assets/crate.jpg");
 
   testScene = new Scene();
   testScene.objects.push(testObject);
